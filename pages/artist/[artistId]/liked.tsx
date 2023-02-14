4c0c88632f4d38f833b8cd4a2db658fef62103e9
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-02-14 23:16:48 +0800

    Protect Another Route

--- pages/artist/[artistId]/liked.tsx
+++ pages/artist/[artistId]/liked.tsx
@@ -1,6 +1,32 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0/dist/frontend";
+/* import { withPageAuthRequired } from "@auth0/nextjs-auth0/dist/frontend"; */
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import ArtistLikedSongs from "../../../components/Artist/ArtistLikedSongs";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  // Create authenticated Supabase Client
+  const supabase = createServerSupabaseClient(ctx);
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+    },
+  };
+};
 
 const Liked = () => {
   return (
@@ -13,4 +39,4 @@ const Liked = () => {
   );
 };
 
-export default withPageAuthRequired(Liked);
+export default Liked;
